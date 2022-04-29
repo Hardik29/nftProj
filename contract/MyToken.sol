466e1dd3f1bbaa294b3f3b269f515c675bf7bcb0
Hardik singh <singhhardik14@gmail.com>
2022-04-29 18:40:28 +0530

    Changed Smart Contract

--- contract/MyToken.sol
+++ contract/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+
 
 
 contract MyToken is ERC1155, Ownable, ERC1155Burnable {
@@ -20,13 +20,11 @@ contract MyToken is ERC1155, Ownable, ERC1155Burnable {
     {
         _mint(account, id, amount,"");
     }
-    
-    function uri(uint256 _tokenid) override public pure returns (string memory) {
-        return string(
-            abi.encodePacked(
-                "https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/",
-                Strings.toString(_tokenid),".json"
-            )
-        );
+    function transfer(address account, uint256 id, uint256 amount)
+        public
+        onlyOwner
+    {
+        _transfer(msg.sender, account, id, amount,"");
     }
+
 }
